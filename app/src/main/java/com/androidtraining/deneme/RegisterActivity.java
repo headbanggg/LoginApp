80d35af497e37b173937af3a28e3b43b5246745a
mine <headbang92@hotmail.com>
2018-12-28 01:04:37 +0300

    register activity updated

--- app/src/main/java/com/androidtraining/deneme/RegisterActivity.java
+++ app/src/main/java/com/androidtraining/deneme/RegisterActivity.java
@@ -3,11 +3,23 @@ package com.androidtraining.deneme;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.textclassifier.TextLinks;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -26,15 +38,37 @@ public class RegisterActivity extends AppCompatActivity {
                 String newUserName = etName.getText().toString();
                 String newUserPass = etPass.getText().toString();
                 int id = rg.getCheckedRadioButtonId();
-                String newFavoriteColor = ((RadioButton) findViewById(id)).getText().toString();
+                final String newFavoriteColor = ((RadioButton) findViewById(id)).getText().toString();
 
-                User newUser = new User(newUserName, newUserPass, newFavoriteColor);
-                if (LoginHelper.addNewUser(newUser)) {
-                    Toast.makeText(getApplicationContext(), "user added " , Toast.LENGTH_LONG).show();
+                RequestQueue requestQueue= Volley.newRequestQueue(RegisterActivity.this);
+                String url="https://egitimlogin.herokuapp.com/user";
 
-                } else {
-                    Toast.makeText(getApplicationContext(), "wrong username " + newFavoriteColor, Toast.LENGTH_LONG).show();
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("username",newUserName);
+                    jsonObject.put("password",newUserPass);
+                    jsonObject.put("favColor", newFavoriteColor);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
+
+
+                JsonObjectRequest jsonObjectRequest =new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("mine"+ response);
+                        Toast.makeText(getApplicationContext(), "user added " , Toast.LENGTH_LONG).show();
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "wrong username " + newFavoriteColor, Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+                requestQueue.add(jsonObjectRequest);
 
             }
         });
